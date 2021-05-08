2b9f09452ac404c08997328e628b5bd552341605
zanedewar <zane_dewar@hotmail.com>
2021-05-08 05:11:41 -0700

    Test

---
+++ testjs.js
@@ -0,0 +1,11 @@
+function searchMenu() {
+	<ul>
+		<li><a href="index.html">Home</a></li>
+		<li><a href="about.html">About Me</a></li>
+		<li><a href="coursework.html">Coursework</a></li>
+		<li><a href="#">Artifacts</a></li>
+		<li><a href="#">Module Assignments</a></li>
+		<li><a href="#">Contact Me</a></li>
+		<li><a href="#">Interests</a><li>
+	</ul>
+}
