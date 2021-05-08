176bc32196b1cd3d8816007219e93a30c032eeb6
zanedewar <zane_dewar@hotmail.com>
2021-05-08 15:22:02 -0700

    background

--- testjs.js
+++ testjs.js
@@ -1,3 +1,10 @@
-$('#searchmenu').load('menu.html' function() {
-	alert('Load was performed');
-});
+var elMenu = document.getElementById('searchmenu');
+elMenu.innerHTML = '<ul>'+
+		'<li><a href="#">Home</a></li>' +
+		'<li><a href="#">About Me</a></li>' +
+		'<li><a href="#">Coursework</a></li>' + 
+		'<li><a href="#">Artifacts</a></li>' + 
+		'<li><a href="#">Module Assignments</a></li>' + 
+		'<li><a href="#">Contact Me</a></li>' +
+		'<li><a href="#">Interests</a><li>' +
+		'</ul>';
